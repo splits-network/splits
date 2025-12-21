5612bc2cb507f401912b93c87ad13072af7b5efc
Brandon Korous <me@brandonkorous.com>
2025-12-20 21:30:08 -0800

    feat: integrate Sentry for corporate app and remove legacy configurations

--- apps/corporate/sentry.client.config.ts
+++
@@ -1,7 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-
-Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || process.env.SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV,
-  tracesSampleRate: 0.1,
-});
