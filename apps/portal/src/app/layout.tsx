98ef963f609e27dd361b574540403da93287d153
Brandon Korous <me@brandonkorous.com>
2026-01-22 08:05:34 -0800

    feat: add Clarity tracking script to candidate and portal layouts

--- apps/portal/src/app/layout.tsx
+++ apps/portal/src/app/layout.tsx
@@ -105,6 +105,18 @@ export default async function RootLayout({
                             }),
                         }}
                     />
+                    <script
+                        type="text/javascript"
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function(c,l,a,r,i,t,y){
+                                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                                })(window, document, "clarity", "script", "v5i71hg3lv");
+                            `,
+                        }}
+                    />
                     <link rel="stylesheet" href="https://kit.fontawesome.com/728c8ddec8.css" crossOrigin="anonymous" />
                 </head>
                 <body className="flex flex-col min-h-screen bg-base-300">
